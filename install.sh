74f62d4dd7c2c8c8c72b6c6d4f64e23f1d8c2c54
Gorman Stock <gstock@roadrunnerwm.com>
2025-02-24 21:30:19 -0600

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-OMP_HOME="/home/coder/.local/bin/oh-my-posh"
-if [ ! -d "$OMP_HOME" ]; then
-  curl --silent --output /dev/null https://ohmyposh.dev/install.sh
-fi
-
-eval "$(/home/coder/.local/bin/oh-my-posh init zsh --config "https://raw.githubusercontent.com/gormanstock/coderdotfiles/refs/heads/main/omp.toml")"
